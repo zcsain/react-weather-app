09208b3b19f80b514f5bfa26bd51fb30460cc150
Zdravko Cicin-Sain <zcsain@gmail.com>
2021-05-17 13:34:40 +0200

    add error view component

--- src/components/views/ErrorView.jsx
+++ src/components/views/ErrorView.jsx
@@ -1,10 +1,63 @@
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+
+// Material UI
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import PriorityHighIcon from "@material-ui/icons/PriorityHigh";
+import ReplayIcon from "@material-ui/icons/Replay";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		maxWidth: "600px",
+	},
+	avatar: {
+		backgroundColor: red[600],
+	},
+}));
 
 function ErrorView() {
+	const classes = useStyles();
+
 	return (
-		<div>
-			<div>Something went wrong, please reload the page or try again later</div>
-		</div>
+		<Card className={classes.root}>
+			<CardHeader
+				avatar={
+					<Avatar className={classes.avatar}>
+						<PriorityHighIcon />
+					</Avatar>
+				}
+				title={<Typography variant="h4">Error</Typography>}
+			/>
+			<CardContent>
+				<Typography variant="body1">
+					Oops! Something went wrong. Please reload the page and try again, or
+					come back later.
+				</Typography>
+			</CardContent>
+			<CardActions>
+				<Grid container item justify="flex-end">
+					<Button
+						variant="contained"
+						color="primary"
+						component={RouterLink}
+						to="/"
+						startIcon={<ReplayIcon />}
+						disableElevation
+					>
+						Reload Page
+					</Button>
+				</Grid>
+			</CardActions>
+		</Card>
 	);
 }
 
